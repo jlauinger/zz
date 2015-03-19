e1c9d4cc89b097a74eb4cab159df307eb74781d0
Johannes Lauinger <johannes@lauinger-it.de>
2015-03-19 01:25:00 +0100

    Create zz.sh
    initial definition of functions

---
+++ zz.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# zz rocket fast directory changer
+# cloned, forked or at least shamelessly borrowed from https://github.com/jlauinger/zz
+# licensed under GPL
+
+alias zz=_zz
+alias zzify=_zzify
+
+function zz {
+  echo not implemented yet, go away
+}
+
+function zzify {
+  echo not implemented yet, go away
+}
